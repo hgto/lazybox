2a0bd4a46e51aeed53d7ed21b4aa94f6a4b843cd
hgto <77352283+hgto@users.noreply.github.com>
2015-07-06 01:52:02 -0400

    adding bspwm, ci lots

---
+++ cli-tools.bash
@@ -0,0 +1,17 @@
+apt-get install vim-gtk\
+  vcsh\
+  htop\
+  powertop\
+  tmux\
+  git\
+  gnupg\
+  ssh\
+  zsh\
+  vim-nox\
+  rsync\
+  nodejs-legacy\
+  npm
+
+chsh -s /bin/zsh `whoami`
+vcsh clone https://github.com/hgto/dots
+vcsh clone https://github.com/hgto/Xdots
